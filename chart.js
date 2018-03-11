965852b4ff1a38ea4eaf954a841b06b5071301ab
Anastasia Tsiga <p15tsig@ionio.gr>
2018-03-11 16:52:41 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -48,7 +48,7 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
-		$("#view-amount-type").fadeOut(250);
+		
 		return total();
 		//location.reload();
 	}
@@ -58,7 +58,7 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeIn(1000);
-		$("#view-amount-type").fadeOut(250);
+		
 		return partyGroup();
 	}
 	if (name === "group-by-donor-type") {
@@ -67,7 +67,7 @@ function transition(name) {
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-donor-type").fadeIn(1000);
-		$("#view-amount-type").fadeOut(250);
+		
 		
 		return donorType();
 	}
@@ -80,15 +80,7 @@ function transition(name) {
 		$("#view-amount-type").fadeOut(250);
 		return fundsType();
 	}
-	if (name === "group-by-amount")
-		$("#initial-content").fadeOut(250);
-		$("#value-scale").fadeOut(250);
-		$("#view-donor-type").fadeOut(250);
-		$("#view-party-type").fadeOut(250);
-		$("#view-source-type").fadeOut(250);
-		$("#view-amount-type").fadeIn(1000);
-		return amountType();
-	}
+	
 
 function start() {
 

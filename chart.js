e0c9d1dc9a2e2132fbe0aebdfa6a2700b0534f34
Anastasia Tsiga <p15tsig@ionio.gr>
2018-03-13 00:51:36 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -260,10 +260,10 @@ function moveToAmountGroup(alpha) {
 		var centreY = svgCentre.y;
 		
 		if (d.value >= 500001) {
-			centreY = svgCentre.y + 75;
+			centreX = svgCentre.x;
 
 		} else if (d.value < 500000) {
-			centreY = svgCentre.y - 25;	
+			centreY = svgCentre.y;	
                 }
 		
 		

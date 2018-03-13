067156e15902129cc974b4d1bdae880c970cc704
Anastasia Tsiga <p15tsig@ionio.gr>
2018-03-13 21:34:03 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -267,8 +267,8 @@ function moveToAmountGroup(alpha) {
                 }
 		
 		
-		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
-		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
+		d.x += (centreX - d.x) * (brake + 0.1) * alpha * 2.2;	
+		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 2.2;
 	};
 }
 
